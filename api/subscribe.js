2de013dd1b2acd52511e788646f79a30b11de4ee
Osekone <154329993+Osekone@users.noreply.github.com>
2026-02-01 02:46:55 +0100

    Update subscribe.js

--- api/subscribe.js
+++ api/subscribe.js
@@ -22,6 +22,7 @@ export default async function handler(req, res) {
                 attributes: {
                     "TAG": "LANDING_V1",
                     "ORIGEN": "Web Oficial"
+                    "FECHA_REGISTRO": new Date().toISOString().split('T')[0] // Formato YYYY-MM-DD que pide Brevo para tipo Fecha
                 }
             })
         });
